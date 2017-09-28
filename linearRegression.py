77822511dac611143689dc9249d59fcfac363c31
Web D.va <webNoDva@gmail.com>
2017-09-28 15:48:21 -0400

    Fixed mistake in calculating standard deviation and used generator expression.

--- linearRegression.py
+++ linearRegression.py
@@ -3,18 +3,9 @@ import math
 # x is the set of x-variables and y the set of y-variables
 def calculateLinearRegressionLine(x, y):
     
-    # First, calculate the variances and standard deviations of each set of variables.
+    # First, calculate the standard deviations of each set of variables.
+    x_standardDeviation = math.sqrt(sum((i - sum(x) / len(x))**2 for i in x) / len(x))
+    y_standardDeviation = math.sqrt(sum((i - sum(y) / len(y))**2 for i in y) / len(y))
     
-    # Calculate the variance.
-    x_mean = sum(x) / len(x)
-    i = 0
-    for j in x:
-        i += (j - x_mean)**2
-    # The standard deviation is the square root of the variance.
-    x_standardDeviation = math.sqrt(i / len(x))
+    # Next, calculate the correlation between the x and y variables.
     
-    y_mean = sum(y) / len(y)
-    i = 0
-    for j in y:
-        i += (j - y_mean)**2
-    y_standardDeviation = math.sqrt(i / len(y))

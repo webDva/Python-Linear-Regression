e08b47b773e0ca62d847d3982c3d375d3bb0951d
Web D.va <webNoDva@gmail.com>
2017-09-29 05:41:24 -0400

    Had trouble with interpreting definition of correlation.

--- linearRegression.py
+++ linearRegression.py
@@ -8,4 +8,18 @@ def calculateLinearRegressionLine(x, y):
     y_standardDeviation = math.sqrt(sum((i - sum(y) / len(y))**2 for i in y) / len(y))
     
     # Next, calculate the correlation between the x and y variables.
+    avg_x = float(sum(x)) / len(x)
+    avg_y = float(sum(y)) / len(y)
+    diffprod = 0
+    xdiff2 = 0
+    ydiff2 = 0
+    for idx in range(len(x)):
+        xdiff = x[idx] - avg_x
+        ydiff = y[idx] - avg_y
+        diffprod += xdiff * ydiff
+        xdiff2 += xdiff * xdiff
+        ydiff2 += ydiff * ydiff
+    r_xy_correlation = diffprod / math.sqrt(xdiff2 * ydiff2)
+    
+    # Using the calculated standard deviations and correleation, calculate the slope of the regression line.
     
